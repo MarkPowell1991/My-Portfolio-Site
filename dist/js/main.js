4bfcef9ea67222346eabf5913185f7e38d8266c7
MarkPowell1991 <markpowell1991@hotmail.co.uk>
2018-12-05 21:17:30 +0000

    more commenting, more structure to the "about me page" and link to first project

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,32 +1,34 @@
 // select DOM items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn"); //gets the menu button
+const menu = document.querySelector(".menu"); //gets the menu itself
+const menuNav = document.querySelector(".menu-nav"); //gets the nav menu panel
+const menuBranding = document.querySelector(".menu-branding"); // gets the branding panel
+const navItems = document.querySelectorAll(".nav-item"); //gets all the nav items
 
 //Set initial State of Menue
 
-let showMenu = false;
+let showMenu = false; //the menu is closed by default
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu); //run togglemenu method when menu button is clicked
 
 function toggleMenu() {
   if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+    //if the menu was closed, flag variables with "show"
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show")); //flags each item with "show"
 
     //set menu state
-    showMenu = true;
+    showMenu = true; //flag the menu as open
   } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    //if the menu was open as the button was clicked, do the oppsoite
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
 
     //set menu state
     showMenu = false;
